6bdbdf3867b933c8f81f0fbf775dced282453202
SavaletDev <savinien.petitjean@gmail.com>
2023-10-07 16:59:32 +0200

    Init

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+###########
+# Imports #
+#######################################################################
+  description = "Pikatsuto dotfiles";
+  # ----------------------------------------------------------------- #
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    ## ------------------------------------------------------------- ##
+    hosts.url = github:StevenBlack/hosts;
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
+    ## ------------------------------------------------------------- ##
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+#############
+# Variables #
+#######################################################################
+  outputs = {
+    self,
+    nixpkgs,
+    nixos-hardware,
+    home-manager,
+    hosts,
+    ...
+  } @inputs: let
+    username = "savalet";
+    system = "x86_64-linux";
+    hostname = "PC-2";
+    ## ------------------------------------------------------------- ##
+    default_modules = [
+      hosts.nixosModule {
+        networking.stevenBlackHosts = {
+          blockFakenews = true;
+          blockGambling = true;
+          blockPorn = false;
+          blockSocial = true;
+        };
+      }
+    ];
+##########
+# Config #
+#######################################################################
+  in
+  {
+    nixosConfigurations = {
+      NixAchu-Fix = nixpkgs.lib.nixosSystem {
+        inherit system;
+        #### ----------------------------------------------------- ####
+        modules = default_modules ++ [
+          (import ./computer.nix {
+            hostname = "NixAchu-Fix";
+            inherit username;
+            inherit home-manager;
+          })
+        ];
+      };
+      ### --------------------------------------------------------- ###
+      NixAchu-Portable = nixpkgs.lib.nixosSystem {
+        inherit system;
+        #### ----------------------------------------------------- ####
+        modules = default_modules ++ [
+          (import ./computer.nix {
+            hostname = "NixAchu-Portable";
+            inherit username;
+            inherit home-manager;
+          })
+          ##### ------------------------------------------------- #####
+          nixos-hardware.nixosModules.asus-battery
+          nixos-hardware.nixosModules.common-cpu-intel
+          nixos-hardware.nixosModules.common-pc
+          nixos-hardware.nixosModules.common-pc-ssd
+        ];
+      };
+    };
+  };
+#######################################################################
+}
